b251fb0efa838fa977b31b735623319b35db6fa4
Otire82 <a.jeanne01@sncf.fr>
2022-11-09 09:56:58 +0100

    challenge interface

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,45 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+    	if (!this.flying) {
+    		this.flying = true;
+    		System.out.printf("%s takes off in the sky.%n", this.getName());
+    	}
+    }
+
+    public void land() {
+		if (this.altitude > 1)
+			System.out.printf("%s ist too high, it can't land.%n", this.getName());
+		else {
+			System.out.printf("%s lands on the ground.%n", this.getName());
+			this.flying = false;
+		}
+    }
+
+    public int ascend(int meters) {
+    	if (this.flying) {
+    		this.altitude =  this.altitude + meters;
+    		System.out.printf("%s flies upward, altitude: %d %n", this.getName(), this.altitude);
+    	}
+    	return this.altitude;
+    }
+    
+    public int descend(int meters) {
+    	if (this.flying) {
+    		this.altitude =  this.altitude - meters;
+    		System.out.printf("%s flies downward, altitude: %d %n", this.getName(), this.altitude);
+    	}
+    	return this.altitude;
+    }
+
+    
+    @Override
+    public void glide() {
+    	if (this.flying)
+    		System.out.printf("glides into the air %n");
     }
 }
